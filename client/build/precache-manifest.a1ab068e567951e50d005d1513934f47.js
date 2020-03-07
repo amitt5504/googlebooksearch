5df7f92eecdb70d24d9c85a59ab22e0e7cf4ee94
Aaron J Mitchell <amitchell1992@knights.ucf.edu>
2020-03-07 14:09:19 -0500

    build completed

---
+++ client/build/precache-manifest.a1ab068e567951e50d005d1513934f47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1b41ad666b806a822a60",
+    "url": "/static/js/main.8f8f6eda.chunk.js"
+  },
+  {
+    "revision": "b4688a03485cd6b44def",
+    "url": "/static/js/2.67da6bea.chunk.js"
+  },
+  {
+    "revision": "1b41ad666b806a822a60",
+    "url": "/static/css/main.7cb0faca.chunk.css"
+  },
+  {
+    "revision": "017dd1c5a1783e94e2422876780be675",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds047437.mlab.com:47437/heroku_v2s80d5g", { useUnifiedTopology: true, useNewUrlParser: true });
 
 // Start the API server
 app.listen(PORT, function() {
